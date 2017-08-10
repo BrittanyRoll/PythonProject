05a90bb2a4ae26bc9488ec77b107e3bc71665402
BrittanyRoll <artemismoon2015@gmail.com>
2017-08-10 15:06:33 -0400

    uses classes

--- social_network.py
+++ social_network.py
@@ -3,8 +3,8 @@ import math
 
 class User:
 
-    def _init_(self, name):
-        self.user_name = name
+    def __init__(self, name):
+        self.user_name = name     #constructor
         self.age = 0
         self.interests = []
         self.friends = []
@@ -13,7 +13,7 @@ class User:
         return self.user_name    #shows name
 
     def change_age(self, age):
-        self.age = age          #change age
+        self.age = int(age)          #change age
 
     def get_age(self):
         return self.age        #shows age
@@ -36,16 +36,81 @@ class User:
 
 
 class Network:
-    def _init_(self, ):
-        self.profile
+    def __init__(self):
         self.all_users = []
 
-    def users(self, friends):
-        
+    def users(self, name):
+        self.all_users.append(name)
+
+    def get_users(self):
+        return self.all_users
+
+    def num_users(self):
+        return (len(self.all_users))
+
+    def add_connect(self, user1, user2):
+        user1.friends(user2)
+        user2.friends(user1)
+
+    def show_connect(self):
+        return (add_connect)
+
 
 
 def main():
+    print("Do you want to make a new user? (enter yes or no)")
+    user_input = input()
+    #key = click.getchar('y')
+    #key1 = click.getchar('n')
+    if user_input == "yes":
+        user1 = User("Brittany")
+        user2 = User("Ericka")
+        network = Network()
+        add_connect(user1, user2)
+        show_connect()
+
+        print("Enter your name")
+        user_input = input()
+        user1.user_name(user_input)
+        user1.get_name()
+        print("What is your age?")
+        user_input = input()
+        user1.age = int(user_input)
+        user1.get_age
+        print("Do you have any interest?")
+
+        if user_input == "yes":
+            while (user_input == "yes"):
+                print("What is one interest?")
+                user_input = input()
+                user1.add_interest(user_input)
+                print("Do you have anymore interests? (yes or no)")
+                user_input = input()
+
+        elif user_input == "no":
+            print("Do you have any friends?")
+            if user_input == "yes":
+                while user_input == "yes":
+                    print("What is one of the friend's names?")
+                    user_input = input()
+                    user1.friends(user_input)
+                    print("Do you have anymore friends?")
+
+
+            else:
+                print
 
 
 
-if _name_ == '_main_'
+
+
+    else:
+        exit()
+
+
+
+
+
+#if _name_ == '_main_'
+
+main()
